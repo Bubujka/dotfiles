c5e704611ec4762d6ee0510d308e1455cbca5d6f
Aleksej Kamynin <zendzirou@gmail.com>
2014-06-15 15:42:49 +0400

    Добавил конфиги вима

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git submodule init
+git submodule update
+
 for file in *; do
   if [ -d "$file" ]; then
     stow "$file"
---
+++ vim/.vim/after/syntax/javascript.vim
@@ -0,0 +1,2 @@
+"syn match jsConstant '[A-Z_][A-Z_]\+' 
+"hi def link jsConstant        Todo
