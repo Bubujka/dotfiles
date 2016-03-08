ec6017deeeb5c03b868189105f7d81a4b9ce884e
Aleksej Kamynin <zendzirou@gmail.com>
2016-03-08 16:28:39 +0300

    Новый установщик

---
+++ install.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Install configs to system"""
+from glob import glob
+from subprocess import call
+from os.path import isdir, expanduser
+import socket
+
+def hostname():
+    """Узнать имя машины, где скрипт запущен"""
+    return socket.gethostname()
+
+def common_configs():
+    """Получить список каталогов общих конфигов"""
+    all_configs = [f for f in glob('*')
+            if isdir(f) and not is_overlay_dir(f)]
+    overlay = [f.replace('@{}'.format(hostname()), '') for f in overlay_configs()]
+    return [f for f in set(all_configs) - set(overlay)]
+
+def overlay_configs():
+    """Получить список конфигов что относятся к этому компу"""
+    return [f for f in glob("*@{}".format(hostname())) if isdir(f)]
+
+def install_configs(pathes):
+    for pth in pathes:
+        print("Installing: {}".format(pth))
+        call(["stow", pth, "-t", expanduser("~/")])
+
+def is_overlay_dir(pth):
+    """Является ли каталог оверлеем?"""
+    return pth.find('@') > -1
+
+def update_git_submodules():
+    """Инициализирует сабмодули конфигов"""
+    call('git submodule init'.split(' '))
+    call('git submodule update'.split(' '))
+
+if __name__ == '__main__':
+    print("Hostname: {}".format(hostname()))
+    update_git_submodules()
+    install_configs(common_configs())
+    install_configs(overlay_configs())
