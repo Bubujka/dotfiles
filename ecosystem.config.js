884b04c36d3560f38d42f39ab478cd41d9d64713
Aleksej Kamynin <zendzirou@gmail.com>
2018-01-07 09:00:34 +0300

    Задержка перед restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'window-activity',
       script: '/home/bubujka/.bu.bin/bin/window_activity.py',
       interpreter: '/home/bubujka/.bu.bin/venv/bin/python',
+      restart_delay: 3000
     },
   ],
 };
