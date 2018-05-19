54e1de32a75778901b4c3a07322bce72e27c7c73
Aleksej Kamynin <zendzirou@gmail.com>
2018-05-19 09:19:32 +0300

    Мелочёвка

--- bashrc/.profile.d/ps.sh
+++ bashrc/.profile.d/ps.sh
@@ -14,7 +14,9 @@ __prompt_command() {
     local BBlu='\[\e[1;34m\]'
     local Pur='\[\e[0;35m\]'
     PS1='\n\[\033[32m\]\u@\h\[\033[36m\] $LANGVM \w\[\033[33m\]$(__git_ps1)\[\033[34m\]\n\n'
-
+    if [ ! -z ${ASCIINEMA_REC+x} ]; then
+        PS1+="🎥 "
+    fi
     if [ $EXIT != 0 ]; then
         PS1+="${Red}${EXIT} \$${RCol} "
     else
