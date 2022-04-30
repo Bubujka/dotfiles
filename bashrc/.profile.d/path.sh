35925bbb4824dc15c44439d58baea9e075d0011b
Aleksej Kamynin <zendzirou@gmail.com>
2022-04-30 13:04:02 +0300

    Правки

--- bashrc/.profile.d/path.sh
+++ bashrc/.profile.d/path.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:$HOME/.rvm/bin
 export PATH=$PATH:$HOME/.omega.bin/bin
 export PATH=$PATH:$HOME/.bu.cards/bin
 export PATH=$PATH:$HOME/.bu.bin/bin
+export PATH=$PATH:$HOME/.db/bu.bin-private/bin
 export PATH=$PATH:$HOME/.beta.bin/bin
 export PATH=$PATH:$HOME/.config/composer/vendor/bin
 export PATH=$PATH:$HOME/.composer/vendor/bin
