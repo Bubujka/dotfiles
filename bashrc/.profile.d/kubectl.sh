c5bc9eca1db882aa8cc2e6c7bb2ce24afe6ae35a
Aleksej Kamynin <zendzirou@gmail.com>
2022-06-20 17:18:43 +0300

    Мелочёвка

--- bashrc/.profile.d/kubectl.sh
+++ bashrc/.profile.d/kubectl.sh
@@ -1,3 +1,21 @@
 source <(kubectl completion bash)
 complete -o default -F __start_kubectl k
 
+kfs() {
+  local dir
+  unset DEBUG;
+
+  if [ $# -eq "0" ] ; then
+    dir=$(kubectl get pods -n stage --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Переход к серверу") && kubectl exec -n stage --stdin --tty "$dir" -- /bin/bash
+  else
+    dir=$(kubectl get pods -n "$1" --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Переход к серверу") && kubectl exec -n "$1" --stdin --tty "$dir" -- /bin/bash
+  fi
+}
+
+kfsn() {
+  local dir
+  unset DEBUG;
+
+  ns=$(kubectl get ns --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Выбор нэймспэйса в кубере")
+  dir=$(kubectl get pods -n "$ns" --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Переход к поду в нэймспэйсе $ns") && kubectl exec -n "$ns" --stdin --tty "$dir" -- /bin/bash
+}
