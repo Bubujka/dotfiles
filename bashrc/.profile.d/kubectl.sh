9d38c90f3b4bed057a08f1d9f995bdab2cde493a
Aleksej Kamynin <zendzirou@gmail.com>
2022-06-25 23:43:09 +0300

    мелочёвка

--- bashrc/.profile.d/kubectl.sh
+++ bashrc/.profile.d/kubectl.sh
@@ -19,3 +19,25 @@ kfsn() {
   ns=$(kubectl get ns --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Выбор нэймспэйса в кубере")
   dir=$(kubectl get pods -n "$ns" --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Переход к поду в нэймспэйсе $ns") && kubectl exec -n "$ns" --stdin --tty "$dir" -- /bin/bash
 }
+
+klogs() {
+  local dir
+  unset DEBUG;
+
+  dir=$(kubectl get pods -n stage --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Переход к серверу") && kubectl logs -f -n stage "$dir"
+
+  echo "Для просмотра логов можно использовать команду:"
+  echo "kubectl logs -f -n \"stage\" \"$dir\""
+}
+
+klogsn() {
+  local dir
+  unset DEBUG;
+
+  ns=$(kubectl get ns --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Выбор нэймспэйса в кубере")
+  dir=$(kubectl get pods -n "$ns" --no-headers -o custom-columns=":metadata.name" | fzf +m --header="Просмотр логов") && kubectl logs -f -n "$ns" "$dir"
+
+  echo -e "\n\n\nДля просмотра логов можно использовать команду:"
+  echo "kubectl logs -f -n \"$ns\" \"$dir\""
+
+}
