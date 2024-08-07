e7f35cc886241ec6fe5d1161b0676f53b34a51f7
Aleksej Kamynin <zendzirou@gmail.com>
2024-08-08 00:11:53 +0300

    Мелочёвка

--- bashrc/.profile.d/ansible.sh
+++ bashrc/.profile.d/ansible.sh
@@ -3,3 +3,29 @@ av() {
   unset DEBUG;
   dir=$(ag ANSIBLE_VAULT -l | fzf +m --header="Открытие зашифрованного файла") && ansible-vault edit "$dir"
 }
+
+
+fap() {
+	echo "Выполняем команду";
+	cat .fap~
+	sleep 1
+	. .fap~
+}
+
+refap() {
+  local dir
+  unset DEBUG;
+  GROUP=$(cat hosts.ini  | grep '^\[' | tr -d '[]' | sort | \
+	  fzf +m --header="Выбор группы серверов" --preview='ansible-hosts-in-group {} | bat --color always |  head -$LINES')
+
+  PLAYBOOK=$(find playbooks -type f -iname '*.yml' | sort | \
+	  fzf +m --header="Выбор плэйбука" --preview='bat --color always {} | head -$LINES')
+
+
+  echo "Команда для запуска (или просто fap)"
+  echo "ansible-playbook -l \"$GROUP\" $PLAYBOOK"
+  echo "ansible-playbook -l \"$GROUP\" $PLAYBOOK" |xclip
+  echo "ansible-playbook -l \"$GROUP\" $PLAYBOOK" > .fap~
+}
+
+
